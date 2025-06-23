4f9370d97787dec01ab2818e44494dd1da6179bc
Phuc Dang <phucsystem@gmail.com>
2025-06-23 22:24:04 +0700

    Dummy commit 1

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -48,7 +48,7 @@ const LoginForm: React.FC = () => {
   return (
     <Container size={1200} my={60}>
       <Title ta="center" fw={900} size="h1" mb="xs">
-        Welcome back!
+        Welcome back phuc 1234!
       </Title>
       <Text c="dimmed" size="lg" ta="center" mb={30}>
         Sign in to your account to continue
